c3366e3f02e04585317be7a539768d473f4ad757
nicolasTawk <nicolas.tawk@std.balamand.edu.lb>
2024-04-23 19:21:10 +0300

    Initial Commit

---
+++ nicolas_tawk&kayan_abdelbaki/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  nicolas_tawk&kayan_abdelbaki
+//
+//  Created by Nicolas Tawk on 23/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
