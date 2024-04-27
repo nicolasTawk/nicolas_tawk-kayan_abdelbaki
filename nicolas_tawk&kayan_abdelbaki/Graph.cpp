0264ea3cb02747cb51863d6065494da4bd52cf08
kayan2004 <76120577+kayan2004@users.noreply.github.com>
2024-04-27 14:25:46 +0300

    Update Graph.cpp

--- nicolas_tawk&kayan_abdelbaki/Graph.cpp
+++ nicolas_tawk&kayan_abdelbaki/Graph.cpp
@@ -1,24 +1,43 @@
-#include "Graph.h"
+// Graph.cpp
 
-Graph::Graph(int V) {
-    this->V = V;
-    adj.resize(V);
+#include "Graph.h"
+#include <iostream>
+
+Vertex::Vertex(int id) : id(id) {}
+
+
+void Graph::addVertex(int id) {
+    vertices.push_back(id);
 }
-void Graph::addVertex() {
-    // Increase the number of vertices and add a new list for the new vertex
-    adj.emplace_back();
-    V++;
+
+
+void Graph::addEdge(int srcId, int destId) {
+    for (auto& vertex : vertices) {
+        if (vertex.id == srcId) {
+            vertex.adjacencyList.push_back({ destId });
+            break;
+        }
+    }
 }
-void Graph::addEdge(int v, int w) {
-    adj[v].push_back(w); 
+
+void Graph::removeEdge(int srcId, int destId) {
+    for (auto& vertex : vertices) {
+        if (vertex.id == srcId) {
+            vertex.adjacencyList.remove_if([destId](const Edge& edge) {
+                return edge.dest == destId;
+                });
+            break;
+        }
+    }
 }
 
 void Graph::printGraph() {
-    for (int v = 0; v < V; ++v) {
-        std::cout << "Adjacency list of vertex " << v << "\n head ";
-        for (auto x : adj[v]) {
-            std::cout << "-> " << x;
+    for (const auto& vertex : vertices) {
+        std::cout << "Vertex " << vertex.id << " connects to: ";
+        for (const auto& edge : vertex.adjacencyList) {
+            std::cout << edge.dest << ", ";
         }
         std::cout << std::endl;
     }
 }
+
