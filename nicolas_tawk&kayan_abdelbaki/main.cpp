e99d78e2df3b185afbcae62213fbdef603c35ce7
kayan2004 <76120577+kayan2004@users.noreply.github.com>
2024-04-27 14:26:41 +0300

    Update main.cpp

--- nicolas_tawk&kayan_abdelbaki/main.cpp
+++ nicolas_tawk&kayan_abdelbaki/main.cpp
@@ -1,9 +1,61 @@
 #include "Graph.h"
+#include "User.h"
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
+int id = 0;
+unordered_map<int, User> users;
+
+int getUserIDByName(string name) {
+    for (const auto& pair : users) {
+        if (pair.second.getName() == name) {
+            return pair.first;
+        }
+    }
+    return -1;
+}
+void addUser(Graph network) {
+    string name;
+    cout << "Enter the name of the user : ";
+    cin >> name;
+
+    users[id] = User(id, name);
+    network.addVertex(id);
+}
+
+void addConnection(Graph network) {
+    string follower, followed;
+    cout << "Enter the name of the follower : ";
+    cin >> follower;
+    cout << "Enter the name of the followed : ";
+    cin >> followed;
+
+    int  follower_id = getUserIDByName(follower);
+    int followed_id = getUserIDByName(followed);
+    if (follower_id != -1 && followed_id != -1) {
+        network.addEdge(follower_id, followed_id);
+    }
+    else {
+        cout << "One or both of the users are not in the network" << endl;
+    }
+
+}
+
+void removeConnection(Graph network) {
+    string follower, followed;
+    cout << "Enter the name of the follower : ";
+    cin >> follower;
+    cout << "Enter the name of the followed : ";
+    cin >> followed;
+
+    int  follower_id = getUserIDByName(follower);
+    int followed_id = getUserIDByName(followed);
+}
 
 void menu() {
     cout << "Enter a number from 1 to 20" << endl;
+    cout << "1: Initialize the network" << endl;
     cout << "1: Add a user to the network " << endl;
     cout << "2: Add a connection to a user" << endl;
     cout << "3: remove a connection from a user" << endl;
@@ -14,39 +66,39 @@ void menu() {
     cout << "8: print the details about a user" << endl;
     cout << "9: get the followers of a user" << endl;
     cout << "10: get the followings of a user" << endl;
+    cout << "11: traverse the network using BFS" << endl;
+    cout << "12: traverse the network using DFS" << endl;
 
     cout << "11: empty the network" << endl;
     cout << "12: exit" << endl;
 }
 
-void handleChoice(int choice) {
+void handleChoice(Graph network, int choice) {
     switch (choice) {
     case 1:
         cout << "hello";
-        //addUser();
+        // addUser(network);
     }
 }
-
-void addUser() {
-    string name;
-    cout << "Enter the name of the user : ";
-    cin >> name;
-
-}
-
 int main() {
+    Graph network =  Graph();
+    User alice(1, "alice");
+    User bob(2, "bob");
+    User sam(3, "sam");
+    
+    network.addVertex(1);
+    network.addVertex(2);
+    network.addVertex(3);
 
-    Graph network(10);
-    Graph g(3); // Create a graph with 3 vertices initially
-    g.addEdge(0, 1); // Connect vertex 0 and 1
+    network.addEdge(1, 2);
+    network.addEdge(1, 3);
 
-    // Adding a new isolated vertex
-    g.addVertex(); // This will be vertex 3
-    g.addVertex(); // This will be vertex 4 (also isolated)
+    network.printGraph();
 
-    g.printGraph(); // Display the graph
+    network.removeEdge(1, 2);
 
-
+    network.printGraph();
+   
 
     int choice;
     do {
@@ -56,10 +108,14 @@ int main() {
             cout << "Make sure the number is between 1 and 12 inclusively" << endl;
             cin >> choice;
         }
-        // handleChoice(choice);
-
-    } while (choice != 12);
+       // handleChoice(choice);
+        
+    }while (choice != 12);
 
 
     return 0;
 }
+
+
+
+
